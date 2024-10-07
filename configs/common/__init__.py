048d0c3a58f4722227043959a548b9ec2d2a0fba
Federico Berto <berto.federico2@gmail.com>
2024-10-08 04:45:08 +0900

    Delete repo files; use only page

--- configs/common/__init__.py
+++
@@ -1,4 +0,0 @@
-from .dqn import *
-from .env import *
-from .test import *
-from .val import *
--- configs/common/val.py
+++
@@ -1,9 +0,0 @@
-from .dqn import save_interval
-
-use_validation = True
-val_interval = save_interval
-val_test_set = ((40, 16, 0.3), (40, 32, 0.3), (40, 64, 0.3))
-
-# cpu cores
-val_pool_parallel_cores = 16
-val_device = "cpu"
